78a598a77037596b90581fe8dff37813928da86e
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-01-12 02:18:25 -0800

    Create JavaCode.java

---
+++ JavaCode.java
@@ -0,0 +1,28 @@
+package BasicJava;
+
+import java.io.*;
+
+public class JavaCode {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream("C:\\Users\\Rakib\\Desktop\\input.txt");
+            out = new FileOutputStream("C:\\Users\\Rakib\\Desktop\\output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+
+            }
+        }
+            finally{
+                if(in!=null){
+                    in.close();
+                }if(out!=null){
+                    out.close();
+                }
+            }
+        }
+    }
