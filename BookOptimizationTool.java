059ad7c9035631065d3353dc6970dc90f6ac57dd
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-01-23 08:34:50 +0600

    Update BookOptimizationTool.java

--- BookOptimizationTool.java
+++ BookOptimizationTool.java
@@ -1,67 +1,90 @@
 package BasicJava;
-
 import java.io.*;
-import java.util.Scanner;
 
-public class InStream {
+import java.io.IOException;
 
-    public static void main(String[] args) throws IOException {
+public class OutStream {
 
-        int input_number ;
-        String space;
-        System.out.println("How many number of books you want to input : ");
+    public static void main(String [] args) throws IOException{
 
-        Scanner scan = new Scanner(System.in);
-        input_number = scan.nextInt();
-        space  = scan.nextLine();
-
-        int iteration;
-        String bookName,writerName,bookID, bookPrice;
-        String bookInfo;
-        File file = new File("C:\\Users\\Rakib\\Desktop\\input.txt");
-        file.createNewFile();
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("bookName\twriterName\tbookID\tbookPrice\n");
-       fileWriter.flush();
-       // fileWriter.close();
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-        for(iteration=0;iteration<input_number;iteration++){
-
-          // fileWriter.append((CharSequence) file);
-            System.out.println("Book Name : ");
-            bookName  = scan.nextLine();
-
-            System.out.println("Writer Name : ");
-            writerName = scan.nextLine();
-
-            System.out.println("Book ID : ");
-            bookID = scan.nextLine();
-
-            System.out.println("Book Price : ");
-            bookPrice = scan.nextLine();
-
-            bookInfo = bookName+"\t"+writerName+"\t"+bookID+"\t"+bookPrice+"\n";
-bufferedWriter.write(bookInfo);
-//bufferedWriter.close();
-           // fileWriter.write(bookInfo);
-           // fileWriter.flush();
-           // fileWriter.close();
-        }
-
-bufferedWriter.close();
-         fileWriter.close();
-
-
-
-
+        File file = new File("C:\\Users\\Rakib\\Desktop\\IIT_SPL.txt");
         FileReader fr = new FileReader(file);
-        char [] a = new char[950];
+        char [] a = new char[12050];
         fr.read(a);   // reads the content to the array
 
-        for(char c : a)
-            System.out.print(c);   // prints the characters one by one
-        fr.close();
+        String bookName [] = new String[1050];
+        String writerName [] = new String[1050];
+        String bookId [] = new String[1050];
+        String bookType [] = new String[1050];
+        String priority [] = new  String[1050];
+        String borrowCount [] = new String[1050];
+        String monthCount [] = new String[1050];
+        int x=1;
+        int t=0;
+      for(int i =0 ;a[i]!='\0';i++) {
+          if(a[i]=='\t'){
+              i++;
+              t++;
+              t=t%7;
+          }
+          if(a[i]=='\n'){
+              i++;
+              t++;
+              t=t%7;
+              x++;
+              bookName [x] = "\0";
+               writerName [x]  = "\0";
+               bookId [x] = "\0";
+               bookType [x] = "\0";
+               priority [x] = "\0";
+               borrowCount [x] ="\0";
+               monthCount [x] = "\0";
 
-    }
+          }
+          if(t==0){
+
+              bookName[x]  =bookName[x] + a[i];
+              System.out.println(bookName[x]);
+          }
+else if(t==1){
+    writerName[x] = writerName[x]+a[i];
+              System.out.println(writerName[x]);
+
+          }
+else if(t==2){
+    bookId[x] = bookId[x]+a[i];
+              System.out.println(bookId[x]);
+
+          }
+     else if(t==3){
+    bookType[x] = bookType[x]+a[i];
+              System.out.println(bookType[x]);
+          }
+     else if(t==4){
+    priority[x] = priority[x]+a[i];
+              System.out.println(priority[x]);
+          }
+     else if(t==5){
+    borrowCount[x] = borrowCount[x]+a[i];
+              System.out.println(borrowCount[x]);
+          }
+     else if(t==6){
+    monthCount[x] = monthCount[x]+a[i];
+              System.out.println(monthCount[x]);
+          }
+
+
+      }
+
+      fr.close();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(monthCount[7]);
+      }
 }
+
+
+
+//Java Code Three
