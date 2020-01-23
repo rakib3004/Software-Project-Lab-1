c3e5f5a2706c4241890fdbc62e1a3a7cf7e310bc
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-01-23 07:40:48 +0600

    Update BookOptimizationTool.java

--- BookOptimizationTool.java
+++ BookOptimizationTool.java
@@ -1,29 +1,59 @@
-
-package BasicJava;
+package JavaFile;
 
 import java.io.*;
+import java.util.Scanner;
 
-public class JavaCode {
+public class InStream {
+
     public static void main(String[] args) throws IOException {
 
-        FileInputStream in = null;
-        FileOutputStream out = null;
-        try {
-            in = new FileInputStream("C:\\Users\\Rakib\\Desktop\\input.txt");
-            out = new FileOutputStream("C:\\Users\\Rakib\\Desktop\\output.txt");
+        int input_number ;
+        String space;
+        System.out.println("How many number of books you want to input : ");
 
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write(c);
+        Scanner scan = new Scanner(System.in);
+        input_number = scan.nextInt();
+         space  = scan.nextLine();
 
-            }
-        }
-            finally{
-                if(in!=null){
-                    in.close();
-                }if(out!=null){
-                    out.close();
-                }
-            }
-        }
+        int iteration;
+        String bookName,writerName,bookID, bookPrice;
+        String bookInfo;
+        File file = new File("C:\\Users\\Rakib\\Desktop\\input.txt");
+        file.createNewFile();
+        FileWriter fileWriter = new FileWriter(file);
+
+          for(iteration=0;iteration<input_number;iteration++){
+
+        System.out.println("Book Name : ");
+        bookName  = scan.nextLine();
+
+        System.out.println("Writer Name : ");
+        writerName = scan.nextLine();
+
+        System.out.println("Book ID : ");
+        bookID = scan.nextLine();
+
+        System.out.println("Book Price : ");
+        bookPrice = scan.nextLine();
+
+        bookInfo = bookName+" "+writerName+" "+bookID+" "+bookPrice+"\n";
+
+              fileWriter.write(bookInfo);
+              fileWriter.flush();
+              fileWriter.close();
+         }
+
+
+
+
+
+        FileReader fr = new FileReader(file);
+        char [] a = new char[950];
+        fr.read(a);   // reads the content to the array
+
+        for(char c : a)
+            System.out.print(c);   // prints the characters one by one
+        fr.close();
+
     }
+}
