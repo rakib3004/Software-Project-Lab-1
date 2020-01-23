2d42e3944bd1573004e909fd741238d2d94456ee
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-01-23 14:11:54 +0600

    Update BookOptimizationTool.java

--- BookOptimizationTool.java
+++ BookOptimizationTool.java
@@ -15,12 +15,19 @@ public class OutStream {
         String bookName [] = new String[1050];
         String writerName [] = new String[1050];
         String bookId [] = new String[1050];
+
         String bookType [] = new String[1050];
+
         String priority [] = new  String[1050];
+        int priority2 [] = new int[1050];
+
         String borrowCount [] = new String[1050];
+        int borrowCount2 [] = new int[1050];
+
         String monthCount [] = new String[1050];
+        int monthCount2 [] = new int[1050];
         int x=1;
-        int t=0;
+        int t=0,t1=1;
       for(int i =0 ;a[i]!='\0';i++) {
           if(a[i]=='\t'){
               i++;
@@ -30,7 +37,21 @@ public class OutStream {
           if(a[i]=='\n'){
               i++;
               t++;
+              if(t==7){
+                  t1=1;
+              }
               t=t%7;
+            //  priority2[x] = Integer.parseInt(priority[x]);
+            //  borrowCount2[x] = Integer.parseInt(borrowCount[x]);
+              System.out.println(bookName[x]);
+              System.out.println(writerName[x]);
+              System.out.println(bookId[x]);
+              System.out.println(bookType[x]);
+              System.out.println(priority[x]);
+              System.out.println(borrowCount[x]);
+              System.out.println(monthCount[x]);
+              System.out.println();
+              System.out.println();
               x++;
               bookName [x] = "\0";
                writerName [x]  = "\0";
@@ -44,33 +65,38 @@ public class OutStream {
           if(t==0){
 
               bookName[x]  =bookName[x] + a[i];
-              System.out.println(bookName[x]);
           }
 else if(t==1){
     writerName[x] = writerName[x]+a[i];
-              System.out.println(writerName[x]);
 
           }
 else if(t==2){
     bookId[x] = bookId[x]+a[i];
-              System.out.println(bookId[x]);
 
           }
      else if(t==3){
     bookType[x] = bookType[x]+a[i];
-              System.out.println(bookType[x]);
+
           }
      else if(t==4){
     priority[x] = priority[x]+a[i];
-              System.out.println(priority[x]);
+
           }
      else if(t==5){
     borrowCount[x] = borrowCount[x]+a[i];
-              System.out.println(borrowCount[x]);
+
           }
      else if(t==6){
-    monthCount[x] = monthCount[x]+a[i];
-              System.out.println(monthCount[x]);
+         if(t1==1){
+             t1++;
+             continue;
+         }
+         else{
+             monthCount[x] = monthCount[x]+a[i];
+
+
+         }
+
           }
 
 
@@ -78,13 +104,37 @@ else if(t==2){
 
       fr.close();
 
+
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(monthCount[7]);
+
+     //   for(int s : monthCount2)
+     //   System.out.println(s);
+        for (int j=0;j<50;j++){
+            System.out.println(bookName[j]);
+
+        } for (int j=0;j<50;j++){
+            System.out.println(writerName[j]);
+
+        } for (int j=0;j<50;j++){
+            System.out.println(bookId[j]);
+
+        } for (int j=0;j<50;j++){
+            System.out.println(priority[j]);
+
+        } for (int j=0;j<50;j++){
+            System.out.println(bookType[j]);
+
+        }
+        for (int j=0;j<50;j++){
+            System.out.println(monthCount[j]);
+              monthCount2[x] = Integer.parseInt(monthCount[x]);
+
+
+        }for (int j=0;j<50;j++){
+            System.out.println(borrowCount[j]);
+
+        }
       }
 }
 
-
-
-//Java Code Three
+//java code five 
