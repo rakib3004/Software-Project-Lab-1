2be90c13f551ef90888008806b142235e2914fa0
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-07 15:29:30 +0600

    Update Analytic.java

--- Analytic.java
+++ Analytic.java
@@ -2,9 +2,8 @@ package IOStream;
 
 public class Analytic {
     public void analysis(BookData[] bookData,String[] writerName,
-                         String[] bookType,String[] priority,String[] borrowCount1,String[] monthCount,
-                         String[] bookPrice1,String[] bookId){
-        //   Processing processing = new Processing();
+                         String[] borrowCount1,String[] bookPrice1,String[] bookId){
+     //   Processing processing = new Processing();
 
         int typeValue [] =new int[1050];
         int timeCount [] = new int[1050];
@@ -13,44 +12,45 @@ public class Analytic {
         int bookPrice [] =new int[1050];
         int writePriority [] = new int[1050];
 
-        String string;
+        String string,string1,string2;
         int length;
-
-        for(int i=0;i<50;i++){
-            if(writerName[i].equals(" Humayon Ahmed")){
-                writePriority[i]=25;
-            }
-            else if(writerName[i].equals("Muhammad Jafar Iqbal")){
+        int integer1,integer2;
+        int newYear,oldYear;
+        for(int i=0;i<200;i++){
+           if(writerName[i].equals(" Humayon Ahmed")){
+               writePriority[i]=25;
+           }
+           else if(writerName[i].equals("Muhammad Jafar Iqbal")){
                 writePriority[i]=23;
             }
             else {
-                writePriority[i]=15;
+               writePriority[i]=15;
             }
 
-        } for(int i=0;i<50;i++){
+        } for(int i=0;i<200;i++){
             length = bookId[i].length();
             string =bookId[i].substring(1,3);
             typeValue[i] = Integer.parseInt(string);
-        } for(int i=0;i<50;i++){
-            length = monthCount[i].length();
-            string =monthCount[i].substring(1,length);
-            timeCount[i] = Integer.parseInt(string);
-        } for(int i=0;i<50;i++){
+        } for(int i=0;i<200;i++){
+
+ newYear = 2 + (20) * 12;
+string1 = bookId[i].substring(4,6);
+integer1 = Integer.parseInt(string1);
+string2=bookId[i].substring(6,8);
+
+integer2 = Integer.parseInt(string2);
+oldYear = integer1+(integer2*12);
+timeCount[i] = newYear - oldYear;
+
+        } for(int i=0;i<200;i++){
             length = borrowCount1[i].length();
             string =borrowCount1[i].substring(1,length);
             bookCount[i] = Integer.parseInt(string);
-        } /*for(int i=0;i<50;i++){
-            length = bookId[i].length();
-            string =bookPrice1[i].substring((length-4),length);
-            queueValue[i] = Integer.parseInt(string);
-        }*/for(int i=0;i<50;i++){
+        }for(int i=0;i<200;i++){
             length = bookPrice1[i].length();
             string =bookPrice1[i].substring(1,length);
             string=string.replaceAll("[\\t\\n\\r]+","");
-            System.out.println(string);
-            if(!string.isEmpty()) {
-                bookPrice[i] = Integer.parseInt(string);
-            }
+            bookPrice[i] = Integer.parseInt(string);
         }
 
         double bookPriority [] = new double[1050];
@@ -63,20 +63,20 @@ public class Analytic {
         //   processing.fileReader();
 
 
-        for(int i =0;i<50;i++){
-            bookPriority[i] =  (20 - Math.sqrt(typeValue[i]));
-            timePriority[i] = 16 -  (timeCount[i]/12);
-            borrowCount[i] = bookCount[i];
+        for(int i =0;i<200;i++){
+bookPriority[i] =  (20 - Math.sqrt(typeValue[i]));
+timePriority[i] = 16 -  (timeCount[i]/12);
+borrowCount[i] = bookCount[i];
 //serialPriority[i] = Math.pow(queueValue[i],(1/3));
-            pricePriority[i] = Math.pow(bookPrice[i],(2/5));
+pricePriority[i] = Math.pow(bookPrice[i],(2/5));
 
-            weight[i] = bookPriority[i] +timePriority[i] +
-                    borrowCount[i]+//serialPriority[i]+
-                    +writePriority[i]+pricePriority[i];
-            System.out.println(weight[i]);
-            bookData[i].setWeight(weight[i]);
+weight[i] = bookPriority[i] +timePriority[i] +
+        borrowCount[i]+//serialPriority[i]+
+        +writePriority[i]+pricePriority[i];
+          //  System.out.println(weight[i]);
+bookData[i].setWeight(weight[i]);
         }
-        Sorting sorting = new Sorting();
+      Sorting sorting = new Sorting();
         sorting.algorithm(bookData);
     }
 
