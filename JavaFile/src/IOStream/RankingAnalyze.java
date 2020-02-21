936fc1bed0cf8461551d113f745a26a6c4d40e33
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-21 12:52:35 +0600

    Add files via upload

--- JavaFile/src/IOStream/RankingAnalyze.java
+++ JavaFile/src/IOStream/RankingAnalyze.java
@@ -4,6 +4,7 @@ public class RankingAnalyze {
     BookData [] bookData1;
  int i,j;
     int number;
+    RankExpose rankExpose = new RankExpose();
    public void rankingAnalyze1(BookData [] bookData, int numberOfBooks){
 
         bookData1 = bookData;
@@ -14,7 +15,10 @@ public class RankingAnalyze {
                dataMean[i] =dataMean[i] + bookData[i].rank[j];
 
            }
+           dataMean[i] = dataMean[i]/9;
        }
+     rankExpose.statitics1(bookData,numberOfBooks,dataMean);
+
    }
    
 
