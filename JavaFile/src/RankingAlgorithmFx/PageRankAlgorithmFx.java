ea9bec7f5d2c548da12ccf94503172fe53faccee
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-04-12 12:54:02 +0600

    create page rank algorithm package and it's class
    create page rank algorithm package and it's class

--- JavaFile/src/RankingAlgorithmFx/PageRankAlgorithmFx.java
+++ JavaFile/src/RankingAlgorithmFx/PageRankAlgorithmFx.java
@@ -1,14 +1,34 @@
 package RankingAlgorithmFx;
 
+import AHPalgorithm.AHPcalculation;
+import AHPalgorithm.AHPprocessImplementation;
+import MainPackage.BookNumber;
+import MainPackage.Processing;
+import ObjectOriented.AHPcriteriaWeight;
+import ObjectOriented.PriorityData;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class PageRankAlgorithmFx extends Application {
 
+    PriorityData[] priorityData;
+    AHPcriteriaWeight ahPcriteriaWeight;
+
+    int numberOfBooks;
+    Processing processing = new Processing();
+    BookNumber bookNumber = new BookNumber();
+
+    AHPcalculation ahPcalculation = new AHPcalculation();
+    AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
 
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
 
+        System.exit(0);
     }
 }
