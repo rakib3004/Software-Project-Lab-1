b40edcceccd2ff7f3a226b77af9fd875ff0f15aa
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-04-14 15:29:13 +0600

    put parameter in methods of check page link class
    put parameter in methods of check page link class

--- JavaFile/src/PageRankAlgorithm/CheckPageLink.java
+++ JavaFile/src/PageRankAlgorithm/CheckPageLink.java
@@ -1,33 +1,35 @@
 package PageRankAlgorithm;
 
+import ObjectOriented.PriorityData;
+
 public class CheckPageLink {
-    public boolean checkPageLinkMethods(){
+    public boolean checkPageLinkMethods(int index, int jIndex,PriorityData[] priorityData, int numberOfBooks){
         boolean linkPageBool = true;
 
         boolean borrowLinkBool,typeLinkBool,writerLinkBool,alphabetLinkBool;
-        borrowLinkBool = checkBorrowLinkBool();
-        typeLinkBool = checkTypeLinkBool();
-        writerLinkBool = checkWriterLinkBool();
-        alphabetLinkBool = checkAlphabetLinkBool();
+        borrowLinkBool = checkBorrowLinkBool(index,jIndex,priorityData,numberOfBooks);
+        typeLinkBool = checkTypeLinkBool(index,jIndex,priorityData,numberOfBooks);
+        writerLinkBool = checkWriterLinkBool(index,jIndex,priorityData,numberOfBooks);
+        alphabetLinkBool = checkAlphabetLinkBool(index,jIndex,priorityData,numberOfBooks);
 
         linkPageBool = (borrowLinkBool&&typeLinkBool&&writerLinkBool&&alphabetLinkBool);
         return linkPageBool;
 
     }
 
-    public boolean checkBorrowLinkBool(){
+    public boolean checkBorrowLinkBool(int index,int jIndex,PriorityData[] priorityData, int numberOfBooks){
         return  true;
 
     }
-     public boolean checkTypeLinkBool(){
+     public boolean checkTypeLinkBool(int index,int jIndex,PriorityData[] priorityData, int numberOfBooks){
          return  true;
 
      }
-     public boolean checkWriterLinkBool(){
+     public boolean checkWriterLinkBool(int index,int jIndex,PriorityData[] priorityData, int numberOfBooks){
          return  true;
 
      }
-     public boolean checkAlphabetLinkBool(){
+     public boolean checkAlphabetLinkBool(int index,int jIndex,PriorityData[] priorityData, int numberOfBooks){
          return  true;
 
      }
