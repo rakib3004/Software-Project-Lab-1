0edfaff5bf638b2cf3110b572fc22d07deed2375
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-04-25 06:29:57 +1200

    complete structure of bar and pie chart class
    complete structure of bar and pie chart class

--- JavaFile/src/VisualRepresentation/InfoPieChart.java
+++ JavaFile/src/VisualRepresentation/InfoPieChart.java
@@ -1,7 +1,6 @@
 package VisualRepresentation;
 
-import BookDataBaseFX.GenericsOfBookFX;
-import BookDataBaseFX.TypesOfBookFX;
+import BookDataBaseFX.*;
 import MainPackage.BookNumber;
 import MainPackage.Processing;
 import ObjectOriented.PriorityData;
@@ -119,7 +118,156 @@ public class InfoPieChart extends Application  {
         primaryStage.show();
     }
 
-    public void startDemandBook(Stage primaryStage) {
+    public void startDemandBook(Stage primaryStage) throws IOException {
+
+
+        Button back = new Button("Back");
+        Button exit = new Button("Exit");
+        back.setTranslateX(0);
+        back.setTranslateY(650);
+        exit.setTranslateX(1100);
+        exit.setTranslateY(650);
+        back.setOnAction(actionEvent -> {
+            DemandsOfBookFX demandsOfBookFX = new DemandsOfBookFX();
+
+            try {
+                demandsOfBookFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+        });
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+
+
+        });
+
+        setStyle(exit);
+        setStyle(back);
+
+
+        back.setPrefSize(200, 80);
+        exit.setPrefSize(200, 80);
+
+
+        String year2008,year2009,year2010,year2011,year2012,year2013,year2014,year2015,year2016,year2017;
+        int  year2008Books,year2009Books,year2010Books,year2011Books,year2012Books,
+                year2013Books,year2014Books,year2015Books,year2016Books,year2017Books;
+        year2008Books=0;year2009Books=0;year2010Books=0;year2011Books=0;
+        year2012Books=0;year2013Books=0;year2014Books=0;year2015Books=0;
+        year2016Books=0;year2017Books=0;
+
+        int iterator;
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
+
+        for(iterator=0;iterator<numberOfBooks;iterator++){
+
+            if(priorityData[iterator].bookData.bookId.contains("17")){
+                year2017Books++;
+            }
+            else if(priorityData[iterator].bookData.bookId.contains("16")){
+                year2016Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("15")){
+                year2015Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("14")){
+                year2014Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("13")){
+                year2013Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0812")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0212")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1211")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0311")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1210")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0810")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0410")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1009")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0409")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1208")){
+                year2008Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0608")){
+                year2008Books++;
+            }
+        }
+
+
+
+        year2008 =  "year2008" ;
+        year2009 =  "year2009" ; year2010 =  "year2010" ; year2011 =  "year2011" ;
+        year2012=   "year2012" ;year2013 =  "year2013" ; year2014 =  "year2014" ;
+        year2015 =  "year2015" ;year2016 =  "year2016" ; year2017 =  "year2017" ;
+
+
+
+
+
+        PieChart pieChart = new PieChart();
+
+        PieChart.Data slice1 = new PieChart.Data(year2008,year2008Books);
+        PieChart.Data slice2 = new PieChart.Data(year2009,year2009Books);
+        PieChart.Data slice3 = new PieChart.Data(year2010,year2010Books);
+        PieChart.Data slice4 = new PieChart.Data(year2011,year2011Books);
+        PieChart.Data slice5 = new PieChart.Data(year2012,year2012Books);
+        PieChart.Data slice6 = new PieChart.Data(year2013,year2013Books);
+        PieChart.Data slice7 = new PieChart.Data(year2014,year2014Books);
+        PieChart.Data slice8 = new PieChart.Data(year2015,year2015Books);
+        PieChart.Data slice9 = new PieChart.Data(year2016,year2016Books);
+        PieChart.Data slice10 = new PieChart.Data(year2017,year2017Books);
+
+        pieChart.getData().add(slice1);
+        pieChart.getData().add(slice2);
+        pieChart.getData().add(slice3);
+        pieChart.getData().add(slice4);
+        pieChart.getData().add(slice5);
+        pieChart.getData().add(slice6);
+        pieChart.getData().add(slice7);
+        pieChart.getData().add(slice8);
+        pieChart.getData().add(slice9);
+        pieChart.getData().add(slice10);
+
+
+        pieChart.setTranslateX(55);
+        pieChart.setTranslateY(55);
+        pieChart.setPrefSize(500,500);
+
+        HBox hBox1 = new HBox(pieChart,back,exit);
+
+
+        VBox vBox3 = new VBox();
+        vBox3.getChildren().addAll(hBox1);
+
+        vBox3.setMaxSize(850, 650);
+        // vBox3.setSpacing(5);
+
+
+        Image background = new Image("libraryBackground4.jpg");
+
+        BackgroundImage bi = new BackgroundImage(background,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background bg = new Background(bi);
+        vBox3.setBackground(bg);
+        Scene scene1 = new Scene(vBox3,900,700);
+
+
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+
 
     }
 
@@ -276,11 +424,307 @@ else if(priorityData[iterator].bookData.bookId.contains("16")){
 
     }
 
-    public void startClassBook(Stage primaryStage) {
+    public void startClassBook(Stage primaryStage) throws IOException {
+
+
+        Button back = new Button("Back");
+        Button exit = new Button("Exit");
+        back.setTranslateX(0);
+        back.setTranslateY(650);
+        exit.setTranslateX(1100);
+        exit.setTranslateY(650);
+        back.setOnAction(actionEvent -> {
+            ClassesOfBookFX classesOfBookFX = new ClassesOfBookFX();
+            try {
+                classesOfBookFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+        });
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+
+
+        });
+
+        setStyle(exit);
+        setStyle(back);
+
+
+        back.setPrefSize(200, 80);
+        exit.setPrefSize(200, 80);
+
+
+        String year2008,year2009,year2010,year2011,year2012,year2013,year2014,year2015,year2016,year2017;
+        int  year2008Books,year2009Books,year2010Books,year2011Books,year2012Books,
+                year2013Books,year2014Books,year2015Books,year2016Books,year2017Books;
+        year2008Books=0;year2009Books=0;year2010Books=0;year2011Books=0;
+        year2012Books=0;year2013Books=0;year2014Books=0;year2015Books=0;
+        year2016Books=0;year2017Books=0;
+
+        int iterator;
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
+
+        for(iterator=0;iterator<numberOfBooks;iterator++){
+
+            if(priorityData[iterator].bookData.bookId.contains("17")){
+                year2017Books++;
+            }
+            else if(priorityData[iterator].bookData.bookId.contains("16")){
+                year2016Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("15")){
+                year2015Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("14")){
+                year2014Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("13")){
+                year2013Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0812")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0212")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1211")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0311")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1210")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0810")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0410")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1009")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0409")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1208")){
+                year2008Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0608")){
+                year2008Books++;
+            }
+        }
+
+
+
+        year2008 =  "year2008" ;
+        year2009 =  "year2009" ; year2010 =  "year2010" ; year2011 =  "year2011" ;
+        year2012=   "year2012" ;year2013 =  "year2013" ; year2014 =  "year2014" ;
+        year2015 =  "year2015" ;year2016 =  "year2016" ; year2017 =  "year2017" ;
+
+
+
+
+
+        PieChart pieChart = new PieChart();
+
+        PieChart.Data slice1 = new PieChart.Data(year2008,year2008Books);
+        PieChart.Data slice2 = new PieChart.Data(year2009,year2009Books);
+        PieChart.Data slice3 = new PieChart.Data(year2010,year2010Books);
+        PieChart.Data slice4 = new PieChart.Data(year2011,year2011Books);
+        PieChart.Data slice5 = new PieChart.Data(year2012,year2012Books);
+        PieChart.Data slice6 = new PieChart.Data(year2013,year2013Books);
+        PieChart.Data slice7 = new PieChart.Data(year2014,year2014Books);
+        PieChart.Data slice8 = new PieChart.Data(year2015,year2015Books);
+        PieChart.Data slice9 = new PieChart.Data(year2016,year2016Books);
+        PieChart.Data slice10 = new PieChart.Data(year2017,year2017Books);
+
+        pieChart.getData().add(slice1);
+        pieChart.getData().add(slice2);
+        pieChart.getData().add(slice3);
+        pieChart.getData().add(slice4);
+        pieChart.getData().add(slice5);
+        pieChart.getData().add(slice6);
+        pieChart.getData().add(slice7);
+        pieChart.getData().add(slice8);
+        pieChart.getData().add(slice9);
+        pieChart.getData().add(slice10);
+
+
+        pieChart.setTranslateX(55);
+        pieChart.setTranslateY(55);
+        pieChart.setPrefSize(500,500);
+
+        HBox hBox1 = new HBox(pieChart,back,exit);
+
+
+        VBox vBox3 = new VBox();
+        vBox3.getChildren().addAll(hBox1);
+
+        vBox3.setMaxSize(850, 650);
+        // vBox3.setSpacing(5);
+
+
+        Image background = new Image("libraryBackground4.jpg");
+
+        BackgroundImage bi = new BackgroundImage(background,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background bg = new Background(bi);
+        vBox3.setBackground(bg);
+        Scene scene1 = new Scene(vBox3,900,700);
+
+
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+
 
     }
 
-    public void startBorrowCount(Stage primaryStage) {
+    public void startBorrowCount(Stage primaryStage) throws IOException {
+
+
+        Button back = new Button("Back");
+        Button exit = new Button("Exit");
+        back.setTranslateX(0);
+        back.setTranslateY(650);
+        exit.setTranslateX(1100);
+        exit.setTranslateY(650);
+        back.setOnAction(actionEvent -> {
+            BorrrowCountOfBookFX borrrowCountOfBookFX = new BorrrowCountOfBookFX();
+            try {
+                borrrowCountOfBookFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+        });
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+
+
+        });
+
+        setStyle(exit);
+        setStyle(back);
+
+
+        back.setPrefSize(200, 80);
+        exit.setPrefSize(200, 80);
+
+
+        String year2008,year2009,year2010,year2011,year2012,year2013,year2014,year2015,year2016,year2017;
+        int  year2008Books,year2009Books,year2010Books,year2011Books,year2012Books,
+                year2013Books,year2014Books,year2015Books,year2016Books,year2017Books;
+        year2008Books=0;year2009Books=0;year2010Books=0;year2011Books=0;
+        year2012Books=0;year2013Books=0;year2014Books=0;year2015Books=0;
+        year2016Books=0;year2017Books=0;
+
+        int iterator;
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
+
+        for(iterator=0;iterator<numberOfBooks;iterator++){
+
+            if(priorityData[iterator].bookData.bookId.contains("17")){
+                year2017Books++;
+            }
+            else if(priorityData[iterator].bookData.bookId.contains("16")){
+                year2016Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("15")){
+                year2015Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("14")){
+                year2014Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("13")){
+                year2013Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0812")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0212")){
+                year2012Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1211")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0311")){
+                year2011Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1210")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0810")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0410")){
+                year2010Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1009")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0409")){
+                year2009Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("1208")){
+                year2008Books++;
+            }else if(priorityData[iterator].bookData.bookId.contains("0608")){
+                year2008Books++;
+            }
+        }
+
+
+
+        year2008 =  "year2008" ;
+        year2009 =  "year2009" ; year2010 =  "year2010" ; year2011 =  "year2011" ;
+        year2012=   "year2012" ;year2013 =  "year2013" ; year2014 =  "year2014" ;
+        year2015 =  "year2015" ;year2016 =  "year2016" ; year2017 =  "year2017" ;
+
+
+
+
+
+        PieChart pieChart = new PieChart();
+
+        PieChart.Data slice1 = new PieChart.Data(year2008,year2008Books);
+        PieChart.Data slice2 = new PieChart.Data(year2009,year2009Books);
+        PieChart.Data slice3 = new PieChart.Data(year2010,year2010Books);
+        PieChart.Data slice4 = new PieChart.Data(year2011,year2011Books);
+        PieChart.Data slice5 = new PieChart.Data(year2012,year2012Books);
+        PieChart.Data slice6 = new PieChart.Data(year2013,year2013Books);
+        PieChart.Data slice7 = new PieChart.Data(year2014,year2014Books);
+        PieChart.Data slice8 = new PieChart.Data(year2015,year2015Books);
+        PieChart.Data slice9 = new PieChart.Data(year2016,year2016Books);
+        PieChart.Data slice10 = new PieChart.Data(year2017,year2017Books);
+
+        pieChart.getData().add(slice1);
+        pieChart.getData().add(slice2);
+        pieChart.getData().add(slice3);
+        pieChart.getData().add(slice4);
+        pieChart.getData().add(slice5);
+        pieChart.getData().add(slice6);
+        pieChart.getData().add(slice7);
+        pieChart.getData().add(slice8);
+        pieChart.getData().add(slice9);
+        pieChart.getData().add(slice10);
+
+
+        pieChart.setTranslateX(55);
+        pieChart.setTranslateY(55);
+        pieChart.setPrefSize(500,500);
+
+        HBox hBox1 = new HBox(pieChart,back,exit);
+
+
+        VBox vBox3 = new VBox();
+        vBox3.getChildren().addAll(hBox1);
+
+        vBox3.setMaxSize(850, 650);
+        // vBox3.setSpacing(5);
+
+
+        Image background = new Image("libraryBackground4.jpg");
+
+        BackgroundImage bi = new BackgroundImage(background,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background bg = new Background(bi);
+        vBox3.setBackground(bg);
+        Scene scene1 = new Scene(vBox3,900,700);
+
+
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+
 
     }
 
