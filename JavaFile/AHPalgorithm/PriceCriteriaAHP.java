ef4e20bbae68a77ac9806abf27e4bb1dcc5a5c66
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-04-26 22:51:17 +1200

    add new packages and delete packages in MLR
    add new packages and delete packages in MLR

---
+++ JavaFile/AHPalgorithm/PriceCriteriaAHP.java
@@ -0,0 +1,86 @@
+package AHPalgorithm;
+
+import ObjectOriented.AHPcriteriaWeight;
+import ObjectOriented.PriorityData;
+
+public class PriceCriteriaAHP {
+    AHPcriteriaWeight ahPcriteriaWeight;
+    AHPSubCriteriaProcess ahpSubCriteriaProcess = new AHPSubCriteriaProcess();
+    double [] priceCounter = new double[3];
+
+    public AHPcriteriaWeight priceCriteriaAHPMethods(double criteria,PriorityData[] priorityData, int numberOfBooks) {
+        double[][] priceCriteriaAHPMatrix = new double[3][3];
+
+
+        int i, j;
+        priceCounter  = ahpSubCriteriaProcess.priceCriteriaCalculationMethods(priorityData,numberOfBooks);
+
+
+        for (i = 0; i < 3; i++) {
+            for (j = i + 1; j < 3; j++) {
+
+                priceCriteriaAHPMatrix[i][j] = priceCounter[i]/priceCounter[j];
+
+            }
+        }
+
+
+         for (i = 0; i < 3; i++) {
+            for (j = i + 1; j < 3; j++) {
+
+                priceCriteriaAHPMatrix[j][i] = Math.pow(priceCriteriaAHPMatrix[i][j], -1);
+
+            }
+        }
+
+        for (i = 0; i < 3; i++) {
+
+            priceCriteriaAHPMatrix[i][i] = 1.00;
+        }
+
+
+        double[] summationMatrix = new double[3];
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+
+                summationMatrix[i] = summationMatrix[i] + priceCriteriaAHPMatrix[i][j];
+
+            }
+
+        }
+
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+
+                priceCriteriaAHPMatrix[i][j] = priceCriteriaAHPMatrix[i][j] / summationMatrix[i];
+            }
+        }
+        double[] priceWeightMatrix = new double[3];
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+
+                priceWeightMatrix[i] = priceWeightMatrix[i] + priceCriteriaAHPMatrix[j][i];
+
+            }
+            priceWeightMatrix[i] = priceWeightMatrix[i] / 3;
+        }
+
+        for (i = 0; i < 3; i++) {
+
+        priceWeightMatrix[i]  =  priceWeightMatrix[i]*criteria;
+
+        }
+
+         ahPcriteriaWeight = new AHPcriteriaWeight(priceWeightMatrix[0],
+                priceWeightMatrix[1],priceWeightMatrix[2]);
+
+        System.out.println(ahPcriteriaWeight.highPrice+"---------->  ahPcriteriaWeight.highPrice");
+        System.out.println(ahPcriteriaWeight.mediumPrice+"---------->  ahPcriteriaWeight.mediumPrice");
+        System.out.println(ahPcriteriaWeight.lowPrice+"---------->  ahPcriteriaWeight.lowPrice");
+        System.out.println();
+       return ahPcriteriaWeight;
+    }
+}
---
+++ JavaFile/VisualRepresentation/PieChartFX.java
@@ -0,0 +1,70 @@
+package VisualRepresentation;
+import MainPackage.BookNumber;
+import MainPackage.Processing;
+import ObjectOriented.PriorityData;
+import Regression.newVersion.TypeCountRegression;
+import javafx.application.Application;
+import javafx.scene.chart.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+import java.io.IOException;
+
+public class PieChartFX extends Application {
+
+    PriorityData[] priorityData;
+
+    int numberOfBooks;
+    Processing processing = new Processing();
+    BookNumber bookNumber = new BookNumber();
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void startTyping(Stage primaryStage) {
+
+
+    }
+
+    public void startTiming(Stage primaryStage) {
+
+    }
+
+
+    public void startBorrowing(Stage primaryStage) {
+
+    }
+
+    public void startPricing(Stage primaryStage) {
+
+    }
+    public Button setStyle(Button button)
+    {
+        button.setStyle("-fx-padding: 8 15 15 15;\n" +
+                "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
+                "    -fx-background-radius: 8;\n" +
+                "    -fx-background-color: \n" +
+                "        linear-gradient(from 0% 93% to 0% 100%, #8d9092 0%, #717375 100%),\n" +
+                "        #8d9092,\n" +
+                "        #717375,\n" +
+                "        radial-gradient(center 50% 50%, radius 100%, #ffffff, #a1a3a6);\n" +
+                "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
+                "    -fx-font-weight: bold;\n" +
+                "    -fx-font-size: 2.1em;");
+        return  button;
+    }
+
+
+}
