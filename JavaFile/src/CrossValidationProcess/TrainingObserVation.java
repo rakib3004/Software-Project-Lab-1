fe02ceabd54a72266e9887c24a3969da3d24bc5d
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-05-11 11:59:19 +0600

    add new class training observation

---
+++ JavaFile/src/CrossValidationProcess/TrainingObserVation.java
@@ -0,0 +1,16 @@
+package CrossValidationProcess;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TrainingObserVation extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
