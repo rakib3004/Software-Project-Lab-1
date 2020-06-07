aa22b8603f32ed26e8e71cbc137c5d24e6bbf9fd
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-06-07 16:59:48 +0600

    remove extra methods in matrix determinant method

--- JavaFile/src/MultiVariableRegression/MatrixDeterminant.java
+++ JavaFile/src/MultiVariableRegression/MatrixDeterminant.java
@@ -6,10 +6,6 @@ public class MatrixDeterminant {
 double determinant;
 int iterator;
    public double calculateDeterminant(double [][] a){
-       String  className = this.getClass().getSimpleName();
-       DateTimeWriter dateTimeWriter =  new DateTimeWriter();
-       dateTimeWriter.dateTimeWriterMethods(className);
-
        for(iterator =0; iterator <3; iterator++) {
            determinant = determinant + (a[0][iterator] * (a[1][(iterator + 1) % 3] * a[2][(iterator + 2) % 3] - a[1][(iterator + 2) % 3] * a[2][(iterator + 1) % 3]));
        }
